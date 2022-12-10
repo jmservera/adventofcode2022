160d7d15f18eac01b9be0545d7749cd155c88a6a
Isabel Cabezas <isacabe@microsoft.com>
2022-12-10 18:07:12 +0100

    isa added here input

--- day01/program/Program.cs
+++ day01/program/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-var elvesFile="../data/elves.txt";
-
-Console.WriteLine($"The result is {new CaloriesCounter(elvesFile).FindMax()}");
+var elvesFile="../data/elvesIsa.txt";
+var elvesFileIsa="../data/elves.txt";
+Console.WriteLine($"The result for Juanma is {new CaloriesCounter(elvesFile).FindMax()}");
+Console.WriteLine($"The result for Isa is {new CaloriesCounter(elvesFileIsa).FindMax()}");

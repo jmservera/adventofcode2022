9a5b9f4c585fbf031329f201dc39cb0c6aa62716
Juan Manuel Servera <8036360+jmservera@users.noreply.github.com>
2022-12-10 22:38:45 +0100

    add jm txt

--- day03/Day03/Day03/Program.cs
+++ day03/Day03/Day03/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Day03;
 
+Console.WriteLine($"The result for Test is {new Rucksack("./data/test.txt").GetSumPriorities()}");
 Console.WriteLine($"The result for Juanma is {new Rucksack("./data/juanma.txt").GetSumPriorities()}");
 Console.WriteLine($"The result for Isa is {new Rucksack("./data/isa.txt").GetSumPriorities()}");
 Console.Read();

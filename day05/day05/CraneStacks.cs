96aaf7712a51ab9f57d6b7ebba17619a5bea7e7d
Isabel Cabezas <isacabe@microsoft.com>
2022-12-11 20:36:26 +0100

    first test: parse stacks works

--- day05/day05/CraneStacks.cs
+++ day05/day05/CraneStacks.cs
@@ -11,10 +11,10 @@ namespace day05
         List<Stack<char>> stacks;
         List<(int, int, int)> moves;
         
-        public CraneStacks(string input)
+        public CraneStacks(string path)
         {
-            stacks = ParseStacks(input);
-            moves = ParseMoves(input);
+            stacks = ParseStacks(path);
+            moves = ParseMoves(path);
         }
 
         public void Move(int from, int to, int nrOfBlocks)
@@ -27,14 +27,82 @@ namespace day05
             return string.Empty;
         }
 
-        public static List<Stack<char>> ParseStacks(string input)
+        public static List<Stack<char>> ParseStacks(string path)
         {
+            var stacks = new List<Stack<char>>();
+
+            var file = System.IO.File.ReadAllLines(path);
+            bool lineIsAStack = true;
+
+            Stack<string> stackFile = new Stack<string>();
+            int counter = 0;
+            while (lineIsAStack && counter <= file.Length)
+            {
+                if (lineIsPartOfStack(file[counter]))
+                {
+                    stackFile.Push(file[counter]);
+                    counter++;
+                }
+                else
+                {
+                    lineIsAStack = false;
+                }
+            }
+            // I have all the stacks in "stack"
+
+            // How many stacks do I have?
+            string lastLine = stackFile.Pop();
+
+            int numStacks = GetNumStacks(lastLine);
+
+            for (int i = 0; i < numStacks; i++)
+            {
+                stacks.Add(new Stack<char>());
+            }
+
+            while (stackFile.Count > 0)
+            { 
+                var line = stackFile.Pop();
+                for (int i = 0; i < numStacks; i++)
+                {
+                    var block = line[1 + 4 * i];
+                    if(block != ' ') 
+                    {
+                        stacks[i].Push(block);
+                    }
+                                        
+                }
+            }
+
+
+
+
+            return stacks;
+        }
+
+        public static List<(int, int, int)> ParseMoves(string path)
+        {
+            var file = System.IO.File.ReadAllLines(path);
+
             return null;
         }
 
-        public static List<(int, int, int)> ParseMoves(string input)
+        private static int GetNumStacks(string lastLine)
         {
-            return null;
+            return int.Parse(lastLine.Split("   ").Last());
+        }
+
+        private static bool lineIsPartOfStack(string line)
+        {
+            if (String.IsNullOrEmpty(line))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+
+            }
         }
 
 

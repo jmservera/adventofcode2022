ba9c425fa0255c82977f4663613ed31f457185c4
Juan Manuel Servera <8036360+jmservera@users.noreply.github.com>
2022-12-11 20:03:50 +0100

    more unit tests

--- day05/day05.Tests/UnitTest1.cs
+++ day05/day05.Tests/UnitTest1.cs
@@ -24,5 +24,37 @@ namespace day05.Tests
             Assert.Equal((2, 2, 1), moves[2]);
             Assert.Equal((1, 1, 2), moves[3]);
         }
+
+        public void TestMove()
+        {
+            var cs = new CraneStacks(path);
+
+            cs.Move(1, 2, 1);
+            
+            Assert.Equal(3, cs.GetStackCopy(0).Count);
+            Assert.Equal(2, cs.GetStackCopy(1).Count);
+            Assert.Equal(1, cs.GetStackCopy(2).Count);
+
+            Assert.Equal("DCP", cs.GetHead());
+
+        }
+
+        [Fact]
+        public void TestCopyStack()
+        {
+            var s = new Stack<char>();
+            s.Push('a');
+            s.Push('b');
+
+
+            var s2 = new Stack<char>(s.Reverse());
+            Assert.Equal('b', s.Peek());
+            s.Pop();
+            Assert.Equal('a', s.Pop());
+
+            Assert.Equal('b', s2.Peek());
+            s2.Pop();
+            Assert.Equal('a', s2.Pop());
+        }
     }
 }
--- day05/day05/CraneStacks.cs
+++ day05/day05/CraneStacks.cs
@@ -10,21 +10,44 @@ namespace day05
     {
         List<Stack<char>> stacks;
         List<(int, int, int)> moves;
-        
+
         public CraneStacks(string input)
         {
             stacks = ParseStacks(input);
             moves = ParseMoves(input);
         }
 
-        public void Move(int from, int to, int nrOfBlocks)
+        public Stack<char> GetStackCopy(int i)
+        {
+            var stack = new Stack<char>(stacks[i].Reverse());
+            return stack;
+        }
+
+        public void Move(int nrOfBlocks, int from, int to)
         {
             
         }
 
+        public string GetHead()
+        {
+            var sb = new StringBuilder();
+            foreach (var stack in stacks)
+            {
+                if (stack.Count > 0)
+                {
+                    sb.Append(stack.Peek());
+                }
+                else
+                {
+                    sb.Append('_');
+                }
+            }
+            return sb.ToString();
+        }
+
         public string RunAllMoves()
         {
-            return string.Empty;
+            return GetHead();
         }
 
         public static List<Stack<char>> ParseStacks(string input)

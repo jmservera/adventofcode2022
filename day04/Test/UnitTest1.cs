5264162928905811e62a56a2d612b371eda77833
Isabel Cabezas <isacabe@microsoft.com>
2022-12-11 18:34:10 +0100

    test 1

---
+++ day04/Test/UnitTest1.cs
@@ -0,0 +1,20 @@
+using day04.data;
+
+namespace Test
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData(2,4,6,8, false)]
+        [InlineData(2, 3, 4, 5, false)]
+        [InlineData(5, 7, 7, 9, false)]
+        [InlineData(2, 8, 3, 7, true)]
+        [InlineData(6, 6, 4, 6, true)]
+        [InlineData(2, 6, 4, 8, false)]
+
+        public void Test1(int a1, int b1, int a2, int b2, bool expectedResult)
+        {
+            Assert.Equal(expectedResult, RangeCheck.ContainRange(a1, b1, a2, b2));
+        }
+    }
+}
---
+++ day04/day04/data/RangeCheck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day04.data
+{
+    public class RangeCheck
+    {
+
+        //two ranges are a1-b1, a2-b2
+        public static bool ContainRange(int a1, int b1, int a2, int b2)
+        {
+
+            return true;
+
+        }
+
+
+    }
+}

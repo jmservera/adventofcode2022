bf35d914102ee3b594a3dab983293a6a900bbe93
Juan Manuel Servera <8036360+jmservera@users.noreply.github.com>
2022-12-12 22:33:32 +0100

    add files

---
+++ day07/day07/FileSystem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day07
+{
+    public class FileSystem
+    {
+        public FileSystem(string file) { }
+
+    }
+}

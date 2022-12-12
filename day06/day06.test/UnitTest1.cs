af68ec6924d74927207c77f78354dd16d79aa189
Isabel Cabezas <isacabe@microsoft.com>
2022-12-12 20:24:07 +0100

    Tests day 6

---
+++ day06/day06.test/UnitTest1.cs
@@ -0,0 +1,30 @@
+namespace day06.test
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData("1234", false)]
+        [InlineData("aqwer", false)]
+        [InlineData("aqaer", false)]
+        [InlineData("aqwuyq", false)]
+        public void allDifferentTest(string sequency, bool result)
+        {
+            Assert.Equal(result, Sequency.checkIfDifferents(sequency));
+        }
+
+        [Theory]
+        [InlineData("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7)]
+        [InlineData("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)]
+        [InlineData("nppdvjthqldpwncqszvftbrmjlhg", 6)]
+        [InlineData("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
+        [InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
+
+        public void findMarkerTest(string sequency, int result)
+        {
+
+            Assert.Equal(result, Sequency.findMarker(sequency));
+        }
+
+
+    }
+}
---
+++ day06/day06/Sequency.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day06
+{
+    public class Sequency
+    {
+        string signal;
+
+        public Sequency(string path) 
+        {
+            signal = System.IO.File.ReadAllLines(path).ToString();
+
+        }
+        public int FindMarkerInSignal()
+        {
+            return FindMarker(signal);
+        }
+        public static int FindMarker(string signal)
+        {
+
+            // TODO: juanma
+            return 4;
+        }
+
+        public static bool checkIfDifferents(string sequency)
+        {
+            for(int i = 0; i< sequency.Length; i++)
+            {
+                for(int j=i; j < sequency.Length; j++)
+                {
+                    if (sequency[i] == sequency[j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true; 
+        }
+    }
+}
